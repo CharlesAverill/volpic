3b6db67429601b62b41155f72f35e51bceacd4b7
Charles Averill <charlesaverill20@gmail.com>
2024-03-18 23:40:54 +0100

    Palindrome modified works for length-1 strings but breaks on anything else

--- theories/palindrome/palindrome_modified.v
+++ theories/palindrome/palindrome_modified.v
@@ -13,6 +13,11 @@ Open Scope Z_scope.
 Open Scope volpic_notation.
 Import ListNotations.
 
+Definition string_subscript (s : string) (idx : Z) : Z :=
+	match String.get (Z.to_nat idx) s with 
+	| None => -1
+	| Some a => Z.of_nat (Ascii.nat_of_ascii a)
+	end.
 
 Definition ispalindrome (VP_store: store) := 
 	let VP_poison := false in
@@ -23,7 +28,7 @@ Definition ispalindrome (VP_store: store) :=
  			(VP_store,true)) in
 	let (VP_store,VP_poison) := 
 		(if (negb VP_poison) then
- 			 (update VP_store "VP_LEN" (VInteger ( get_int (length VP_store) "VP_result" )),VP_poison) 
+ 			 (update VP_store "VP_LEN" (VInteger (Z.of_nat (String.length (get_string VP_store "VP_STR")))),VP_poison) 
 		else
  			(VP_store,true)) in
 	let (VP_store,VP_poison) := 
@@ -33,7 +38,8 @@ Definition ispalindrome (VP_store: store) :=
 		let bounds_op := (if going_up then Z.leb else Z.geb) in
 		let iter_op := (if going_up then (Z.add 1) else (Z.sub 1)) in
 		let VP_store := update VP_store "VP_I" (VInteger ( 1 )) in
-while ( fun VP_store => bounds_op (get_int VP_store "VP_I") (get_int VP_store "VP_LEN" / 2) ) with VP_store upto 1000%nat begin fun VP_store => 			let VP_store := if (subscript (get_string VP_store "VP_STR") (get_int VP_store "VP_I") 0 !=? subscript (get_string VP_store "VP_STR") (get_int VP_store "VP_LEN" - get_int VP_store "VP_I" + 1) 0) then
+while ( fun VP_store => bounds_op (get_int VP_store "VP_I") (get_int VP_store "VP_LEN" / 2) ) with VP_store upto 1000%nat begin fun VP_store => 			
+let VP_store := if (string_subscript (get_string VP_store "VP_STR") (get_int VP_store "VP_I") !=? string_subscript (get_string VP_store "VP_STR") (get_int VP_store "VP_LEN" - get_int VP_store "VP_I" + 1)) then
  	 ((*Block: next 2 statements*)
 			let VP_store := update VP_store "VP_result" (VInteger ( 0 )) in
 			let VP_broken := true in VP_store) 
@@ -46,6 +52,9 @@ else
 		else
  			(VP_store,true)) in
 VP_store.
+
+Compute ispalindrome (update fresh_store "VP_STR" (VString "aa")) "VP_result".
+
 Extraction "palindrome.ml" ispalindrome.
 
 (*Failed to convert $main: Couldn't find key "value" in data list*)
